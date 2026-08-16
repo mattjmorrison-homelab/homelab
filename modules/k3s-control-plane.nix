4dd262695ac205da65f5de6795e8c8e76fa3a699
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2026-08-16 06:41:39 +0000

    add ports

--- modules/k3s-control-plane.nix
+++ modules/k3s-control-plane.nix
@@ -11,7 +11,10 @@
   # Point tools (kubectl, k9s) at the k3s kubeconfig without per-user setup
   environment.variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
 
-  # Workers and external clients reach the API server on this port
-  networking.firewall.allowedTCPPorts = [ 53 6443 8080 443 ];
-  networking.firewall.allowedUDPPorts = [ 53 ];
+  # 53/tcp,udp: CoreDNS (cluster DNS, queried by pods and nodes)
+  # 6443/tcp: k3s API server, reached by workers and external clients
+  # 10250/tcp: kubelet API (node-to-node)
+  # 8472/udp: flannel VXLAN (node-to-node)
+  networking.firewall.allowedTCPPorts = [ 53 6443 8080 443 10250 ];
+  networking.firewall.allowedUDPPorts = [ 53 8472 ];
 }
--- tests/k3s-control-plane.nix
+++ tests/k3s-control-plane.nix
@@ -37,5 +37,11 @@
 
     with subtest("port 443 is open in firewall"):
         machine.succeed("iptables -L nixos-fw -n | grep -w 443")
+
+    with subtest("port 8472 UDP is open in firewall"):
+        machine.succeed("iptables -L nixos-fw -n | grep udp.*8472 || iptables -L nixos-fw -n | grep 8472")
+
+    with subtest("port 10250 TCP is open in firewall"):
+        machine.succeed("iptables -L nixos-fw -n | grep 10250")
   '';
 }
